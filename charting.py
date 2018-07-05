50bafab4a6d3593085d994c9b56d032ea0dd9115
Matthew Mercuri <mercurimatthew@gmail.com>
2018-07-05 01:19:45 -0400

    Changed charting.py and historical_data.py

--- charting.py
+++ charting.py
@@ -1,47 +1,26 @@
-from historical_data import get_stock_dict
-import matplotlib
-import numpy as np
-from matplotlib.dates import DateFormatter
+import historical_data
 import matplotlib.pyplot as plt
-from math import floor, ceil
+import pandas as pd
 
-#helper function that takes a list produce list of dates
-def _date_handling(list):
-    dates = [matplotlib.dates.datestr2num(str(key)) for key in list]
-    return dates
+plt.style.use("ggplot")
 
-#helper function that takes a list to produce list of prices
-def _price_handling(list):
-    prices = [round(float(value),2) for value in list]
-    return prices
+def stockgraph(ticker, past_days=90):
 
-#helper function that takes a ticker and returns stock data
-def _chart_data(ticker):
-    #use ticker to grab historical data from historical_data.py function
-    stock_data = get_stock_dict(ticker)
-    dates = np.asarray(_date_handling(stock_data.keys()))
-    prices = np.asarray(_price_handling(stock_data.values()))
-    #flipping order of array, why is it reverse?
-    dates = dates[::-1]
-    prices = prices[::-1]
-    return dates, prices
+    PAST_DAYS = past_days
 
-def draw_graph(ticker, past_trading_days=100):
-    dates, prices = _chart_data(ticker)
-    #grabs data for the defined amount of trading days
-    dates = dates[-(past_trading_days):]
-    prices = prices[-(past_trading_days):]
-    fig, ax = plt.subplots()
-    ax.plot_date(x=dates, y=prices, fmt="r-")
-    plt.yticks(np.arange(floor(min(prices)), ceil((max(prices)) + 2), 1.0))
-    formatter = DateFormatter("%d-%m-%y")
-    ax.xaxis.set_major_formatter(formatter)
-    fig.autofmt_xdate()
+    df = historicaldata.daily_adjusted(ticker)
+
+    x = df.index.tolist()
+    y = df.values.tolist()
+    x = x[-PAST_DAYS:]
+    y = y[-PAST_DAYS:]
+
+    plt.plot(x,y)
+    plt.xlabel("Date")
+    plt.xticks(rotation=90)
+    plt.ylabel("Price")
     plt.title(ticker)
-    fig.canvas.set_window_title(ticker)
-    plt.xlabel("Price")
-    plt.ylabel("Date")
     plt.show()
 
-#FOR TESTING:
-#draw_graph("AAPL")
+
+#stockgraph("AAPL", 90)
--- historical_data.py
+++ historical_data.py
@@ -1,45 +1,40 @@
-import urllib.request
 import json
+import pandas as pd
+import requests
 
-QUERY_URL = "https://www.alphavantage.co/query?function={REQUEST_TYPE}&apikey={KEY}&symbol={SYMBOL}&outputsize=full"
-API_KEY = "GET YOUR API KEY"
 
-#used to make API calls
-def _request(symbol, req_type):
-    with urllib.request.urlopen(QUERY_URL.format(REQUEST_TYPE=req_type, KEY=API_KEY, SYMBOL=symbol)) as req:
-        data = req.read().decode("UTF-8")
+_BASE_URL = "https://www.alphavantage.co/query?function={REQUEST_TYPE}&symbol={TICKER}&outputsize={OUTPUT_SIZE}&apikey={KEY}"
+_API_KEY = ""
+
+def _request(function, ticker, size="full"):
+
+    if function == "daily":
+        action = "TIME_SERIES_DAILY"
+    elif function == "adaily":
+        action = "TIME_SERIES_DAILY_ADJUSTED"
+    else:
+        print("Please idicate if you would like adjusted or non-adjusted data!")
+
+    _completed_url = _BASE_URL.format(REQUEST_TYPE=action, KEY=_API_KEY, OUTPUT_SIZE=size, TICKER=ticker)
+    r = (requests.get(_completed_url)).json()
+    data = json.loads(json.dumps(r))
+
+    stock_data = data["Time Series (Daily)"]
+    for item in stock_data:
+        close_price = stock_data[item]["4. close"]
+        stock_data[item] = close_price
+
+    df = pd.DataFrame.from_dict(stock_data, orient="index")
+    df = df[::-1] #reversing the dataframe
+    df = df.astype(float)
+
+    return df
+
+
+def daily(ticker):
+    data = _request("daily", ticker)
     return data
 
-#inputting a stock ticker as a string, it will output a dict of daily data adjusted
-def _get_daily_data_adjusted(symbol):
-    stock_data = json.loads(_request(symbol, 'TIME_SERIES_DAILY_ADJUSTED'))
-    stock_data_format = stock_data["Time Series (Daily)"]
-    daily_data_adjusted_dict = []
-    for data in stock_data_format.items():
-        daily_data_adjusted_dict.append(data)
-    return daily_data_adjusted_dict
-
-#inputting a stock ticker as a string, it will output a dict of daily data
-def _get_daily_data(symbol):
-    stock_data = json.loads(_request(symbol, 'TIME_SERIES_DAILY'))
-    stock_data_format = stock_data["Time Series (Daily)"]
-    daily_data_dict = []
-    for data in stock_data_format.items():
-        daily_data_dict.append(data)
-    return daily_data_dict
-
-#given a stock ticker, will return a dict of market dates and closing prices
-def get_stock_dict(symbol, adjusted=True):
-    if adjusted == True:
-        requested_stock_data = _get_daily_data_adjusted(symbol)
-    elif adjusted == False:
-        requested_stock_data = _get_daily_data(symbol)
-    else:
-        print("Please indicate if you would like non-adjusted data!")
-    day = []
-    close_price = []
-    for market_day in requested_stock_data:
-        day.append(market_day[0])
-        close_price.append(market_day[1]['4. close'])
-    requested_stock_dict = dict(zip(day, close_price))
-    return requested_stock_dict
+def daily_adjusted(ticker):
+    data = _request("adaily", ticker)
+    return data
